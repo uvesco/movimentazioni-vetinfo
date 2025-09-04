47ac574124dfdb01e74d86965835daecaabeef93
Umberto Vesco <umberto.vesco@yahoo.it>
2025-09-04 22:35:57 +0100

    errore di parentesi

--- R/app_server.R
+++ R/app_server.R
@@ -6,26 +6,31 @@ app_server <- function(input, output, session) {
 	
 	df_specie <- read.csv("data_static/specie.csv", stringsAsFactors = FALSE)
 	
-	# ottieni il numero di righe dei dati importati
-	# 
+	gruppo <- reactive({
+		req(animali())
+		df <- animali()
+		determinare_gruppo(df, df_specie)
+	})
+	
 	output$tipo_file <- renderText({
 		df <- animali()
 		if (is.null(df)) {
 			"File non ancora caricato"
 		} else {
 			tryCatch({
-				gruppo <- determinare_gruppo(df, df_specie)
-				if(gruppo == "vuoto") {
-					return(paste0("File vuoto: "), colnames(df)[1])
+				if(gruppo() == "vuoto") {
+					return(paste0("File vuoto: ", colnames(df)[1]))
 				}else{
-				paste("File importato correttamente. Gruppo specie:", gruppo)
-				}
-			}, error = function(e) {
+				paste("File importato correttamente. Gruppo specie:", gruppo())
+				}, error = function(e) {
 				paste("Errore nel file:", e$message)
 			
 			})
 		}
 	})
+	
+	
+	# ottieni il numero di righe dei dati importati
 	
 	output$n_animali <- renderText({
 		df <- animali()
