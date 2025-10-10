3e9b4d59e8aa675cbe9a052f2e9798f8bba1e9c0
Umberto Vesco <umberto.vesco@yahoo.it>
2025-10-10 18:11:48 +0100

    revisione generazione province indenni

--- src/genera_template_province.R
+++ src/genera_template_province.R
@@ -1,6 +1,7 @@
 # genera template province attuali per aggiornamento indennità riprendendo le indennità vecchie quando attivabili
 
 # pacchetti
+library(utils)
 library(httr2)
 library(jsonlite)
 # library(dplyr)
@@ -21,12 +22,48 @@ url_64 <- paste0("https://situas-servizi.istat.it/publish/reportspooljson?",
 
 df_prov_att  <- fromJSON(url_64, simplifyDataFrame = TRUE)$resultset    # province/UTS con SIGLE (attuali)
 
-indenni <- read_excel("data_static/prov_indenni.xlsx")
+files <- dir(file.path("data_static", "malattie"), pattern = "\\.xlsx$", full.names = TRUE)
 
-indenni <- merge(df_prov_att, indenni[, c(1, 13:15)], by= "COD_PROV_STORICO", all.x = T)
+choice <- menu(files, title = "Seleziona un file")
+
+# Controlla che la scelta sia valida
+if (choice == 0) stop("Nessuna selezione effettuata.")
+
+indenni <- read_excel(files[choice])
+
+
+# i campi indenne devono sempre iniziare per 'Ind_'
+colonne_indenni <- grep("^Ind_", colnames(indenni), value = TRUE)
+# campo chiave da ritenere: "COD_PROV_STORICO"
+
+indenni <- merge(df_prov_att, 
+								 indenni[, c("COD_PROV_STORICO", 
+								 						colonne_indenni
+								 						)], 
+								 by= "COD_PROV_STORICO", 
+								 all.x = T)
 
 # editare il file aggiornato alle province nuove e esportarlo nuovamente  
 # aggiungere un foglio con nella cella A1 la versione del REGOLAMENTO DI ESECUZIONE (UE) 2021/620 DELLA COMMISSIONE
 # https://eur-lex.europa.eu/eli/reg_impl/2021/620
 
-write.xlsx(indenni, "data_static/prov_indenni_template.xlsx")
+# genero dataframe metadati con campi: colonna, malattia, specie, riferimento, data_inizio, data_fine
+metadata <- data.frame(campo = colonne_indenni, 
+											 malattia = as.character(rep(NA, length(colonne_indenni))),
+											 specie = as.character(rep(NA, length(colonne_indenni))),
+											 riferimento = as.character(rep(NA, length(colonne_indenni))),
+											 data_inizio = as.Date(rep(NA, length(colonne_indenni))),
+											 data_fine = as.Date(rep(NA, length(colonne_indenni))),
+											 stringsAsFactors = FALSE)
+
+# se non esiste la cartella templates la crea, la cartella templates è ignorata da git
+
+if(!exists("data_static/malattie_template")){
+	dir.create("data_static/malattie_template")
+}
+
+write.xlsx(list(province = indenni,
+								metadati = metadata), 
+					 file = file.path("data_static", "malattie_template",
+					 								 basename(files[choice])),
+						overwrite = TRUE)
