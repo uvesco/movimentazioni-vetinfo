07882abedb4fec800b3f1683feb63c6e7c9ab72b
Umberto Vesco <umberto.vesco@gmail.com>
2025-11-07 14:51:34 +0100

    importazione dati fissi in global

--- global.R
+++ global.R
@@ -2,3 +2,21 @@ STATIC_SPECIE <- read.csv(
   file = "data_static/decodifiche/specie.csv",
   stringsAsFactors = FALSE
 )
+
+# importazione motivi di ingresso
+STATIC_MOTIVI_INGRESSO <- read.csv(
+  file = "data_static/decodifiche/motivi_ingresso.csv",
+  stringsAsFactors = FALSE
+)
+
+# importazione province italiane
+STATIC_PROVINCE <- read.csv(
+	  file = "data_static/geo/df_province.csv",
+	  stringsAsFactors = FALSE
+)
+
+# importazione prefissi stabilimenti
+STATIC_CODICI_STABILIMENTO <- read.csv(
+  file = "data_static/df_prefissi_stab.csv",
+  stringsAsFactors = FALSE
+)
