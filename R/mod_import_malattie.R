903b4e777286f9b38b360f56c7b4b7dad23d0358
Umberto Vesco <umberto.vesco@gmail.com>
2025-11-17 16:15:35 +0100

    importazione malattie

--- R/mod_import_malattie.R
+++ R/mod_import_malattie.R
@@ -12,7 +12,7 @@ mod_import_malattie <- function(id, gruppo) {
 		reactive({
 			# valore reattivo restituito
 			# req(animali())                                 # richiede che i dati siano presenti
-			req(gruppo())                                  # richiede che il gruppo sia definito
+			# req(gruppo())                                  # richiede che il gruppo sia definito
 			
 
 			
@@ -76,61 +76,99 @@ mod_import_malattie <- function(id, gruppo) {
 					specie = character(0),
 					riferimento = character(0),
 					data_inizio = structure(numeric(0), class = "Date"),
-					data_fine = structure(numeric(0), class = "Date")
+					data_fine = structure(numeric(0), class = "Date"),
+					file = character(0)
 				),
 				row.names = integer(0),
 				class = "data.frame"
 			)
 			###### dataframe da popolare con i dati delle malattie nel ciclo for #####
-			df_province_malattie <- df_province[, c("COD_UTS", "COD_REG")]
-			df_comuni_malattie <- df_comuni[, c("PRO_COM_T", "COD_UTS", "COD_REG")]
+			# df_province_malattie <- df_province[, c("COD_UTS", "COD_REG")]
+			# df_comuni_malattie <- df_comuni[, c("PRO_COM_T", "COD_UTS", "COD_REG")]
 			
 			df_province_malattie_template <- df_province[, c("COD_UTS", "COD_REG")]
 			df_comuni_malattie_template <- df_comuni[, c("PRO_COM_T", "COD_UTS", "COD_REG")]
 			
+			# importazione metadati da tutti i files ------------------------
+			for (i in 1:length(files_malattie)) {
+				file <- files_malattie[i]
+				metadati <- readxl::read_excel(file, sheet = "metadati", col_types = meta_col_types)
+				metadati$file <- file
+				df_meta_malattie <- rbind(df_meta_malattie, metadati)
+			}
+			rm(metadati, file, i)
+			# creo una lista che contiene: il dataframe delle malattie, una lista per gruppo di specie che contiene un dataframe per provincia e uno per comune
+			malattie <- list()
+			
+			# seleziono solo le malattie rilevanti alla data odierna (così non processo files "scaduti")
+			data_odierna <- Sys.Date()
+			df_meta_malattie <- df_meta_malattie %>%
+				filter((is.na(data_inizio) | data_inizio <= data_odierna) &
+							 (is.na(data_fine) | data_fine >= data_odierna)
+							 )
+			# se ci sono duplicati nella malattia per lo stesso gruppo di specie -> errore
+			if (any(duplicated(df_meta_malattie[, c("malattia", "specie")]))) {
+				duplicati <- df_meta_malattie[duplicated(df_meta_malattie[, c("malattia", "specie")]) |
+																				duplicated(df_meta_malattie[, c("malattia", "specie")], fromLast = TRUE), ]
+				stop(
+					"Attenzione: sono stati riscontrati duplicati nei metadati delle malattie per la malattia '",
+					duplicati$malattia[1],
+					"' e il gruppo '",
+					duplicati$specie[1],
+					"'. Controllare il file ",
+					basename(duplicati$file[1])
+				)
+			}
+			# salvo i metadati nella lista malattie
+			
+			malattie[["metadati"]] <- df_meta_malattie
+			
+			# inizializzo i dataframe delle malattie per ogni specie
+			
+			for(i in unique(df_meta_malattie$specie)){
+				malattie[[i]] <- list()
+				malattie[[i]][["province"]] <- df_province_malattie_template
+				malattie[[i]][["comuni"]] <- df_comuni_malattie_template
+			}
+
 			
 			#############################################################
 			# caricamento dei dati delle malattie <- indenne si => TRUE #
 			#############################################################
 			
-			for (i in 1:length(files_malattie)) {
-				file <- files_malattie[i]
+			for (i in 1:nrow(df_meta_malattie))
+					 {
+				file <- df_meta_malattie$file[i]
 				fogli <- tolower(trimws(openxlsx::getSheetNames(file)))  # fogli effettivi del file
-				#### il foglio metadati c'è sempre e mi serve dopo ####
-				metadati <- readxl::read_excel(file, sheet = "metadati", col_types = meta_col_types)
-				
+				# #### il foglio metadati c'è sempre e mi serve dopo ####
+				# metadati <- readxl::read_excel(file, sheet = "metadati", col_types = meta_col_types)
+				# 
 				if (setequal(fogli, tipi_files_malattie_fogli[["province_indenni"]])) {
+
 					########################################
-					# === file con province + metadati === #
-					########################################
+					# file con province + metadati ------
+
 					provinceind <- openxlsx::read.xlsx(file, sheet = "province")
-					# (aggiunge con join al dataframe df_province le colonne indicate in metadati$campo
+					# (aggiunge con join al dataframe delle province della malattia il campo indenne)
 					message("File ",
 									basename(file),
 									" riconosciuto come 'province_indenni'")
-					provinceind <- provinceind %>%
-						mutate(across(
-							starts_with("ind_"),
-							~ ifelse(
-								is.na(.x),
-								NA,
-								tolower(trimws(.x)) %in% c("s", "si", "1", "t", "true")
-							)
-						))
-					
-					df_province_malattie <-
+					provinceind <- provinceind[, c("COD_UTS", df_meta_malattie$campo[i])]
+						
+					# inserisco nel dataframe delle malattie nella lista corretta
+					malattie[[df_meta_malattie$specie[i]]][["province"]] <-
 						merge(
-							df_province_malattie,
-							provinceind[, c("COD_UTS", metadati$campo[metadati$specie == gruppo()])],
+							malattie[[df_meta_malattie$specie[i]]][["province"]],
+							provinceind,
 							by = "COD_UTS",
 							all.x = TRUE,
 							all.y = FALSE
 						)
-					
-					df_comuni_malattie <-
+					# inserisco anche nel dataframe dei comuni (per calcolo successivo)
+					malattie[[df_meta_malattie$specie[i]]][["comuni"]] <-
 						merge(
-							df_comuni_malattie,
-							provinceind[, c("COD_UTS", metadati$campo[metadati$specie == gruppo()])],
+							malattie[[df_meta_malattie$specie[i]]][["comuni"]],
+							provinceind,
 							by = "COD_UTS",
 							all.x = TRUE,
 							all.y = FALSE
@@ -138,10 +176,10 @@ mod_import_malattie <- function(id, gruppo) {
 					
 				} else if (setequal(fogli, tipi_files_malattie_fogli[["blocchi"]])) {
 					##################################################################
-					# === file con blocchi (regioni, province, comuni, metadati) === #
-					##################################################################
-					if (metadati$specie == gruppo()) {
-						# c'è un solo gruppo specie per file
+					
+					# file con blocchi (regioni, province, comuni, metad) ----
+
+
 						# porto a booleano il campo blocco
 						
 						###### ATTENZIONE AI VALORI BOOLEANI T/F (indenne vs bloccato) ######
@@ -207,13 +245,16 @@ mod_import_malattie <- function(id, gruppo) {
 						# qui nego il valore booleano: se regione/provincia/comune è TRUE -> è indenne -> blocco = FALSE #
 						###################################################################################################
 						
-						campo_malattia <- paste0("ind_", metadati$campo)
+						campo_malattia <- df_meta_malattie$campo[i]
 						
 						df_comuni_malattie_temp[, campo_malattia] <- 	with(
 							df_comuni_malattie_temp,!(blocco_regione |
 																					blocco_provincia |
 																					blocco_comune)
 						)
+						
+						# controllo valori NA
+						
 						if (any(is.na(df_comuni_malattie_temp[, campo_malattia]))) {
 							stop(
 								"Attenzione: valori NA riscontrati nel file ",
@@ -225,8 +266,9 @@ mod_import_malattie <- function(id, gruppo) {
 							)
 						}
 						
-						df_comuni_malattie <- merge(
-							df_comuni_malattie,
+						malattie[[df_meta_malattie$specie[i]]][["comuni"]] <-
+							merge(
+								malattie[[df_meta_malattie$specie[i]]][["comuni"]],
 							df_comuni_malattie_temp[, c("PRO_COM_T", campo_malattia)],
 							by = "PRO_COM_T",
 							all.x = TRUE,
@@ -250,8 +292,10 @@ mod_import_malattie <- function(id, gruppo) {
 								# AND su tutti i comuni della provincia
 								.groups = "drop"
 							)
-						df_province_malattie <- merge(
-							df_province_malattie,
+						# ora unisco con il blocco delle province
+						malattie[[df_meta_malattie$specie[i]]][["province"]] <- 
+							merge(
+								malattie[[df_meta_malattie$specie[i]]][["province"]],
 							df_province_malattie_temp[, c("COD_UTS", campo_malattia)],
 							by = "COD_UTS",
 							all.x = TRUE,
@@ -267,25 +311,24 @@ mod_import_malattie <- function(id, gruppo) {
 								metadati$specie
 							)
 							#######################################################################################
-							# df_province_malattie è l'output finale con tutte le malattie a livello di provincia #
+							# la lista malattie è l'output finale con tutte le malattie a livello di provincia #
 							#######################################################################################
+							
+						
 						}
 						
 						
 						# (accoda ai dataframe degli animali)
 						
 						
-					} else {
-						# === file non riconosciuto ===
-						stop("struttura fogli non riconosciuta in ",
-								 basename(file))
-						print(fogli)
+					
 					}
 					
 				}
 				
-			}
-		})
+		}
+			# restituisco la lista malattie
+			malattie
+		)
 	})
 }
-
